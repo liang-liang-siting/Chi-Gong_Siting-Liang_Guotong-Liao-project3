1cdeb8c974c00003b71f5e6b17dc95ad8958f5ea
liangst0721 <liang.sit@northeastern.edu>
2024-04-18 16:13:01 -0700

    add Cryptographically Secure Passwords feature

--- src/PasswordManager.jsx
+++ src/PasswordManager.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './passwordManager.css'; 
 import PasswordStorageFile from './PasswordStorageFile';
 import { useNavigate } from 'react-router-dom';
+import crypto from 'crypto';
 
 function PasswordManager({ isAuthenticated, handleLogout }) {
   const navigate = useNavigate();
@@ -21,12 +22,51 @@ function PasswordManager({ isAuthenticated, handleLogout }) {
   const [submitSuccess, setSubmitSuccess] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredPasswords, setFilteredPasswords] = useState([]);
+  const [useSecurePassword, setUseSecurePassword] = useState(false); 
 
+  const generateSecurePassword = (length) => {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    const charsetLength = charset.length;
+    let password = "";
+  
+    const randomValues = new Uint32Array(length);
+    window.crypto.getRandomValues(randomValues);
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = randomValues[i] % charsetLength;
+      password += charset[randomIndex];
+    }
+  
+    return password;
+  };
+  
   const handleLogoutClick = () => {
     if (handleLogout) {
-      handleLogout(); // Call the handleLogout function passed as prop
+      handleLogout(); 
     }
-    navigate('/login'); // Redirect to the login page
+    navigate('/login'); 
+  };
+
+  const isValidUrl = (url) => {
+    try {
+      new URL(url);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  const handleToggleSecurePassword = () => {
+    setUseSecurePassword(!useSecurePassword);
+
+    // If toggled to use secure password, generate one and set it directly to the password field
+    if (!useSecurePassword) {
+      const securePassword = generateSecurePassword(length);
+      setPassword(securePassword);
+    } else {
+      // Reset password field if secure password is turned off
+      setPassword('');
+    }
   };
 
   const handleSubmit = async () => {
@@ -34,20 +74,27 @@ function PasswordManager({ isAuthenticated, handleLogout }) {
     if (!url.trim()) {
       alert('Please enter a URL');
       return;
+    } else if (!isValidUrl(url)) { // Validate URL format
+      alert('Please enter a valid URL');
+      return;
     }
   
     // If password is not provided, generate a random one
     if (!password.trim()) {
       // Check if at least one checkbox is checked
-      if (!alphabet && !numerics && !symbols) {
+      if (!useSecurePassword && !alphabet && !numerics && !symbols) {
         alert('Please select at least one option for password generation');
         return;
       }
   
       // Check if length is within the range of 4 to 50
-      if (length < 4 || length > 50) {
+      if (!useSecurePassword && (length < 4 || length > 50)) {
         alert('Password length must be between 4 and 50 characters');
         return;
+      }
+  
+      if (useSecurePassword) {
+        return; // No need to generate a password here, already filled by handleToggleSecurePassword
       }
   
       // Generate the random password based on selected options
@@ -56,48 +103,47 @@ function PasswordManager({ isAuthenticated, handleLogout }) {
       if (alphabet) options.push('abcdefghijklmnopqrstuvwxyz');
       if (numerics) options.push('0123456789');
       if (symbols) options.push('!@#$%^&*()_+~`|}{[]:;?><,./-=');
-      
+  
       for (let i = 0; i < length; i++) {
         const randomOption = options[Math.floor(Math.random() * options.length)];
         generatedPassword += randomOption[Math.floor(Math.random() * randomOption.length)];
       }
-      
+  
       setPassword(generatedPassword);
-      return;
-    }
+    } else {
+      // If password is provided, proceed with storing the data
+      try {
+        const response = await fetch('http://localhost:8000/api/passwords/add', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            serviceName: url,
+            password: password
+          }),
+        });
   
-    // If password is provided, proceed with storing the data
-    try {
-      const response = await fetch('http://localhost:8000/api/passwords/add', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          serviceName: url,
-          password: password
-        }),
-      });
-  
-      if (response.ok) {
-        console.log('Password added successfully');
-        setUrl('');
-        setPassword('');
-        setSubmitSuccess(true);
-        window.location.reload();
-        setTimeout(() => {
-          setUpdateSuccess(false);
-        }, 1000);
-      } else {
-        console.error('Failed to add password:', response.statusText);
+        if (response.ok) {
+          console.log('Password added successfully');
+          setUrl('');
+          setPassword('');
+          setSubmitSuccess(true);
+          window.location.reload();
+          setTimeout(() => {
+            setSubmitSuccess(false);
+          }, 1000);
+        } else {
+          console.error('Failed to add password:', response.statusText);
+          alert('An error occurred while adding the password');
+        }
+      } catch (error) {
+        console.error('Error adding password:', error);
         alert('An error occurred while adding the password');
       }
-    } catch (error) {
-      console.error('Error adding password:', error);
-      alert('An error occurred while adding the password');
     }
   };
-
+  
 
   // Function to delete a password file entry
   const handleDeletePasswordFile = (urlToDelete) => {
@@ -156,8 +202,6 @@ function PasswordManager({ isAuthenticated, handleLogout }) {
     setFilteredPasswords(filtered);
   }, [searchQuery, passwords]);
 
-  
-
   return (
     <div className="password-container">
       <h2 className="password-title">Password Manager</h2>
@@ -181,35 +225,51 @@ function PasswordManager({ isAuthenticated, handleLogout }) {
           {showPassword ? "Hide" : "Show"}
         </button>
       </div>
+      {/* Secure Password Toggle */}
       <div className="password-input-row">
-        <div className="checkbox-group">
-          <label className="checkbox-label">
-            <input type="checkbox" checked={alphabet} onChange={() => setAlphabet(!alphabet)} />
-            Alphabet
-          </label>
-          <label className="checkbox-label">
-            <input type="checkbox" checked={numerics} onChange={() => setNumerics(!numerics)} />
-            Numerics
-          </label>
-          <label className="checkbox-label">
-            <input type="checkbox" checked={symbols} onChange={() => setSymbols(!symbols)} />
-            Symbols
-          </label>
+        <div>
+          <label htmlFor="securePasswordToggle" className="secure-password-toggle-label">Use Secure Password:</label>
+          <input
+            type="checkbox"
+            id="securePasswordToggle"
+            checked={useSecurePassword}
+            onChange={handleToggleSecurePassword}
+            className="secure-password-toggle"
+          />
         </div>
-        <input
-          type="number"
-          placeholder="Length"
-          value={length}
-          onChange={(e) => setLength(e.target.value)}
-          className="password-input"
-        />
-        <button onClick={handleSubmit} className="submit-button">Generate Password</button>
       </div>
+      {/* Checkbox Group */}
+      {!useSecurePassword && (
+        <div className="password-input-row">
+          <div className="checkbox-group">
+            <label className="checkbox-label">
+              <input type="checkbox" checked={alphabet} onChange={() => setAlphabet(!alphabet)} />
+              Alphabet
+            </label>
+            <label className="checkbox-label">
+              <input type="checkbox" checked={numerics} onChange={() => setNumerics(!numerics)} />
+              Numerics
+            </label>
+            <label className="checkbox-label">
+              <input type="checkbox" checked={symbols} onChange={() => setSymbols(!symbols)} />
+              Symbols
+            </label>
+          </div>
+          <input
+            type="number"
+            placeholder="Length"
+            value={length}
+            onChange={(e) => setLength(e.target.value)}
+            className="password-input"
+          />
+          <button onClick={handleSubmit} className="submit-button">Generate Password</button>
+        </div>
+      )}
       {/* Submit button */}
       <div className="password-input-row">
         <button onClick={handleSubmit} className="submit-button">Submit</button>
       </div>
-       {/* Password storage file section */}
+      {/* Password storage file section */}
       <div className="password-storage-section">
         <h3>Password Storage Files:</h3>
         <input
@@ -219,7 +279,6 @@ function PasswordManager({ isAuthenticated, handleLogout }) {
           onChange={(e) => setSearchQuery(e.target.value)}
           className="search-bar"
         />
-        
         {filteredPasswords.map((file, index) => (
           <PasswordStorageFile
             key={index}
@@ -239,7 +298,7 @@ function PasswordManager({ isAuthenticated, handleLogout }) {
             onUpdate={handleUpdatePasswordFile}
           />
         ))}
-        </div>
+      </div>
       {/* Sharing section */}
       <div className="password-sharing-section">
         <h3>Share Passwords:</h3>
