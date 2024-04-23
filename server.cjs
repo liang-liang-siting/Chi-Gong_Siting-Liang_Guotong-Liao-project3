ddbeff9f3ed28ad5f4972815308f821567795f9d
Kevin990001 <79775196+Kevin990001@users.noreply.github.com>
2024-04-22 20:16:05 -0700

    fix port

--- server.cjs
+++ server.cjs
@@ -39,6 +39,6 @@ app.put('/', function(request, response) {
     response.send("This is a PUT request")
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(port, '0.0.0.0', () => {
+    console.log(`App listening on port ${port}`)
   })
